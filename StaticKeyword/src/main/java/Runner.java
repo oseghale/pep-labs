cd4a2ee8f3b89a81c7027bd12e3652a2af94e1fa
oseghale <ebeaemmanuel@gmail.com>
2023-01-16 02:05:45 +0000

    Saved progress on StaticKeyword 2023-01-16 02:05:45 +0000

--- StaticKeyword/src/main/java/Runner.java
+++ StaticKeyword/src/main/java/Runner.java
@@ -1,5 +1,7 @@
 
 public class Runner {
+    
+
     /**
      * This challenge is about interacting with static variables.
      *
@@ -22,6 +24,7 @@ public class Runner {
      * @param in the String value you should change global_var to in.
      */
     public void changeClassVar(String in){
+        Class.global_var = in; 
 
     }
 }
