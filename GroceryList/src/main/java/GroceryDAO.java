2daea2aa08393584350e98d2a3e7186862acfacd
oseghale <ebeaemmanuel@gmail.com>
2023-01-19 02:40:54 +0000

    Saved progress on GroceryList 2023-01-19 02:40:54 +0000

--- GroceryList/src/main/java/GroceryDAO.java
+++ GroceryList/src/main/java/GroceryDAO.java
@@ -28,7 +28,7 @@ public class GroceryDAO {
         List<String> groceries = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "select * from Grocery";
             PreparedStatement ps = connection.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
@@ -48,7 +48,7 @@ public class GroceryDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "insert into Grocery (grocery_name) values '" +groceryName+ "';";
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.executeUpdate();
         }catch(SQLException e){
