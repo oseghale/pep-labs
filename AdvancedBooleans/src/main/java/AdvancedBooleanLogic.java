e5b190780468e65967f5576bf0478dd8abc9d577
oseghale <ebeaemmanuel@gmail.com>
2023-02-15 17:12:23 +0000

    Saved progress on AdvancedBooleans 2023-02-15 17:12:23 +0000

--- AdvancedBooleans/src/main/java/AdvancedBooleanLogic.java
+++ AdvancedBooleans/src/main/java/AdvancedBooleanLogic.java
@@ -15,18 +15,28 @@ public class AdvancedBooleanLogic {
      * @param b2 second boolean to be compared.
      * @return false if b1 and b2 are both true. Otherwise, return true.
      */
-    public boolean returnNotBoth(boolean b1, boolean b2){
-        return false;
+    public boolean returnNotBoth(boolean b1, boolean b2) {
+        if (b1 && b2) {
+            return false; // b1 and b2 are both true
+        } else {
+            return true; // b1 and b2 are not both true
+        }
     }
+    
     /**
      * b1 OR b2, BUT NOT BOTH
      * @param b1 first boolean to be compared.
      * @param b2 second boolean to be compared.
      * @return true if either b1 or b2 is true, but false if they are both true or both false.
      */
-    public boolean returnExclusiveOr(boolean b1, boolean b2){
-        return false;
+    public boolean returnExclusiveOr(boolean b1, boolean b2) {
+        if (b1 ^ b2) {
+            return true; // either b1 or b2 is true
+        } else {
+            return false; // both b1 and b2 are true or both are false
+        }
     }
+    
 
     /**
      * Neither b1 NOR b2
@@ -34,7 +44,12 @@ public class AdvancedBooleanLogic {
      * @param b2 second boolean to be compared.
      * @return true if neither b1 nor b2 are true. Return false if either b1 or b2 is true.
      */
-    public boolean returnNeitherNor(boolean b1, boolean b2){
-        return false;
+    public boolean returnNeitherNor(boolean b1, boolean b2) {
+        if (!b1 && !b2) {
+            return true; // neither b1 nor b2 is true
+        } else {
+            return false; // either b1 or b2 is true
+        }
     }
+    
 }
