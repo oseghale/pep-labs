56320805ebf2c45077acb7895296c2f6bfe269b6
oseghale <ebeaemmanuel@gmail.com>
2023-02-15 20:22:23 +0000

    Saved progress on ReverseAString 2023-02-15 20:22:23 +0000

--- ReverseAString/src/main/java/StringReverse.java
+++ ReverseAString/src/main/java/StringReverse.java
@@ -12,7 +12,9 @@ public class StringReverse {
      * @param str a String.
      * @return the reverse of str.
      */
-    public String reverse(String str){
-        return null;
+    public String reverse(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
     }
+    
 }
